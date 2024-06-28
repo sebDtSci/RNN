c91e5c6147b3e243eec068d77be35c92ce4bad48
sebDtSci <sebastientadiello@gmail.com>
2024-06-28 17:45:32 +0200

    regresdsion avoid

--- test/test_src.py
+++ test/test_src.py
@@ -3,5 +3,5 @@ from src.Utils.tensorialAlgebra import *
 def test_tensor():
     assert shape([[1, 2], [3, 4]]) == (2, 2)
     # assert shape(random_uniform(2, 3, 4)) == [2, 3, 4]
-    assert shape(random_normal(2, 3, mean=10)) == [2,3]
+    assert shape(random_normal(2, 3, mean=10)) == (2,3)
     assert tensor_shape([[1, 2], [3, 4]]) == [2, 2]
